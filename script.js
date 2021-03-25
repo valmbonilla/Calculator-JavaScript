0de37f851a835498a0caf52078cda8bd814f6720
vmb <valmbonilla@gmail.com>
2021-03-25 15:55:24 -0700

    defining functions of the calculator

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+class Calculator {
+    constructor (previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.allClear
+    }
+} //gives a way to set the text elements inside of our class //
+ //need to define our functions of the calculator - all our operations to perform
+
+ //clear out our different variables 
+ allClear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined 
+ }
+//removing a single number
+allDelete(){
+
+}
+//adds a number to the screen 
+appendNumber(number) {
+    this.currentOperand = number;
+}
+//this is what happens anytime a user clicks on a function 
+chooseOperation(operation) {
+
+}
+//takes our values to compute a single value to display on our calculator 
+compute() {
+
+}
+//this will update the values inside of our output
+updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperandT
+}
+
 const numberButtons = document.querySelectorAll('[calculator-number]')
 const performanceButtons = document.querySelectorAll('[calculator-performance]')
 const equalsButton = document.querySelector('[calculator-equals]')
@@ -6,12 +42,12 @@ const allClearButton = document.querySelector('[calculator-all-clear]')
 const previousOperandTextElement = document.querySelector('[calculator-previous-operand]')
 const currentOperandTextElement = document.querySelector('[calculator-current-operand]')
 
-class Calculator {
-    
-}
+const calculator = new Calculator (previousOperandTextElement, currentOperandTextElement)
 
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
 
-// 16:39 
-https://www.youtube.com/watch?v=j59qQ7YWLxw
-https://github.com/WebDevSimplified/Vanilla-JavaScript-Calculator/blob/master/styles.css
-//
