9fed67356430f3ac3c9e81f5391dc6076e0a9baf
vmb <valmbonilla@gmail.com>
2021-03-25 16:31:16 -0700

    defining functions in calculator

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ class Calculator {
     constructor (previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
-        this.allClear
+        this.allClear()
     }
-} //gives a way to set the text elements inside of our class //
+ //gives a way to set the text elements inside of our class //
  //need to define our functions of the calculator - all our operations to perform
 
  //clear out our different variables 
- allClear() {
+allClear() {
     this.currentOperand = ''
     this.previousOperand = ''
     this.operation = undefined 
@@ -19,7 +19,7 @@ allDelete(){
 }
 //adds a number to the screen 
 appendNumber(number) {
-    this.currentOperand = number;
+    this.currentOperand = number
 }
 //this is what happens anytime a user clicks on a function 
 chooseOperation(operation) {
@@ -31,9 +31,9 @@ compute() {
 }
 //this will update the values inside of our output
 updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperandT
+    this.currentOperandTextElement.innerText = this.currentOperand
 }
-
+}
 const numberButtons = document.querySelectorAll('[calculator-number]')
 const performanceButtons = document.querySelectorAll('[calculator-performance]')
 const equalsButton = document.querySelector('[calculator-equals]')
@@ -42,7 +42,8 @@ const allClearButton = document.querySelector('[calculator-all-clear]')
 const previousOperandTextElement = document.querySelector('[calculator-previous-operand]')
 const currentOperandTextElement = document.querySelector('[calculator-current-operand]')
 
-const calculator = new Calculator (previousOperandTextElement, currentOperandTextElement)
+const calculator = new Calculator (previousOperandTextElement, 
+    currentOperandTextElement)
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -51,3 +52,14 @@ numberButtons.forEach(button => {
     })
 })
 
+
+// const val = {
+//     getClothes = () => {
+// console.log("clothes")
+//     },
+//     getCoffee = () => {
+//         console.log("get coffee")
+//     }
+// }
+
+// val.getCoffee();
