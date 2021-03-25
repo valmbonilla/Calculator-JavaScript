ad5b3cf5a16e037031dc03eb68059119a1b3a355
vmb <valmbonilla@gmail.com>
2021-03-24 20:52:51 -0700

    class Calculator

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const deleteButton = document.querySelector('[calculator-delete]')
 const allClearButton = document.querySelector('[calculator-all-clear]')
 const previousOperandTextElement = document.querySelector('[calculator-previous-operand]')
 const currentOperandTextElement = document.querySelector('[calculator-current-operand]')
+
+class Calculator {
+    
+}
+
+
+// 16:39 
+https://www.youtube.com/watch?v=j59qQ7YWLxw
+https://github.com/WebDevSimplified/Vanilla-JavaScript-Calculator/blob/master/styles.css
+//
